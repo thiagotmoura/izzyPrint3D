712cd997083e69f1358fccab941e9fb2f5613b19
Thiago Moura <thiago.moura@atlastechnol.com>
2024-02-19 21:37:18 -0300

    feat: creat: empty state component

--- components/EmptyState.vue
+++ components/EmptyState.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="mt-10 text-center">
+  <p class="mt-20 text-center">
     <h2>Nenhum modelo 3D encontrado para esta busca.</h2>
     Teste refazer a busca por outro nome.
   </p>
